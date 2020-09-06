679c57b6a55a1f2d3fac587e2d063d3cd01fbfbc
LuceroRA <lucero.rodrigoa@gmail.com>
2020-09-05 23:17:20 -0400

    Implemented password hashing on user entity

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Model\Entity;
 
+use Authentication\PasswordHasher\DefaultPasswordHasher;
 use Cake\ORM\Entity;
 
 /**
@@ -43,4 +44,15 @@ class User extends Entity
     protected $_hidden = [
         'password',
     ];
+
+    /**
+     * Setter method called by CakePHP whenever 'password' is set in a user entity.
+     * 
+     * @param $password New password
+     */
+    protected function _setPassword(string $password) : ?string {
+        if(strlen($password) > 0){
+            return (new DefaultPasswordHasher())->hash($password);
+        }
+    }
 }
